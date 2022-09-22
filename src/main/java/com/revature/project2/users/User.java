133c16e6ad15c638ef4cd1be237a9930ca9f2617
Luka Licheli <lukalicheli@gmail.com>
2022-09-22 14:19:50 -0700

    Test

--- src/main/java/com/revature/project2/users/User.java
+++ src/main/java/com/revature/project2/users/User.java
@@ -11,25 +11,25 @@ public class User {
     @Id // indicates a primary key
     @Column // all fields in an entity implicitly have @Column
     private UUID user_id;
-    
+
     @Column(unique = true, nullable = false)
     private String username;
-    
+
     @Column(unique = true, nullable = false)
     private String email;
-    
+
     @Column(nullable = false)
     private String password;
-    
+
     @Column(name = "given_name", nullable = false)
     private String givenName;
-    
+
     @Column(nullable = false)
     private String surname;
-    
+
     @Column(name = "is_active")
     private boolean isActive;
-    
+
     @ManyToOne
     @JoinColumn(name = "role_id", nullable = false)
     private Role role;
@@ -42,7 +42,6 @@ public class User {
 
     public User(UUID id, String givenName, String surname, String email, String username, String password, Role role, boolean isActive) {
         this.user_id = id;
-
         this.givenName = givenName;
         this.surname = surname;
         this.email = email;
@@ -109,79 +108,44 @@ public class User {
     public void setRole(Role role) {
         this.role = role;
     }
-    
+
     public boolean isIsActive() {
-            return isActive;
-        }
+        return isActive;
+    }
 
     public void setIsActive(boolean isActive) {
         this.isActive = isActive;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return isActive == user.isActive && user_id.equals(user.user_id) && username.equals(user.username) && email.equals(user.email) && password.equals(user.password) && givenName.equals(user.givenName) && surname.equals(user.surname) && role.equals(user.role);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(user_id, username, email, password, givenName, surname, isActive, role);
+    }
 
     @Override
     public String toString() {
         return "User{" +
-                "user_id='" + user_id + '\'' +
+                "user_id=" + user_id +
+                ", username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
                 ", givenName='" + givenName + '\'' +
                 ", surname='" + surname + '\'' +
-                ", email='" + email + '\'' +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
+                ", isActive=" + isActive +
                 ", role=" + role +
                 '}';
     }
-    
-    @Override
-    public int hashCode() {
-        int hash = 7;
-        hash = 89 * hash + Objects.hashCode(this.user_id);
-        hash = 89 * hash + Objects.hashCode(this.username);
-        hash = 89 * hash + Objects.hashCode(this.email);
-        hash = 89 * hash + Objects.hashCode(this.password);
-        hash = 89 * hash + Objects.hashCode(this.givenName);
-        hash = 89 * hash + Objects.hashCode(this.surname);
-        hash = 89 * hash + (this.isActive ? 1 : 0);
-        hash = 89 * hash + Objects.hashCode(this.role);
-        return hash;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj) {
-            return true;
-        }
-        if (obj == null) {
-            return false;
-        }
-        if (getClass() != obj.getClass()) {
-            return false;
-        }
-        final User other = (User) obj;
-        if (this.isActive != other.isActive) {
-            return false;
-        }
-        if (!Objects.equals(this.username, other.username)) {
-            return false;
-        }
-        if (!Objects.equals(this.email, other.email)) {
-            return false;
-        }
-        if (!Objects.equals(this.password, other.password)) {
-            return false;
-        }
-        if (!Objects.equals(this.givenName, other.givenName)) {
-            return false;
-        }
-        if (!Objects.equals(this.surname, other.surname)) {
-            return false;
-        }
-        if (!Objects.equals(this.user_id, other.user_id)) {
-            return false;
-        }
-        if (!Objects.equals(this.role, other.role)) {
-            return false;
-        }
-        return true;
-
-        return Objects.hash(user_id, givenName, surname, email, username, password, role);
-    }
 }
+
+
+
+
