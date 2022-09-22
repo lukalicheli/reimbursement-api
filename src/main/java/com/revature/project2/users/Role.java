8a87f4bd18d09a4cc7e7bdebb0d627b34e1652a4
Chaz Corlew <chaz.corlew@gmail.com>
2022-09-22 14:42:21 -0700

    updated role.java to reflect table in projects

--- src/main/java/com/revature/project2/users/Role.java
+++ src/main/java/com/revature/project2/users/Role.java
@@ -8,11 +8,11 @@ import java.util.Objects;
 import java.util.UUID;
 
 @Entity
-@Table(name = "user_roles")
+@Table(name = "ers_user_roles")
 public class Role {
 
     @Id
-    private UUID id;
+    private UUID role_id;
 
     @Column(name = "role", nullable = false, unique = true)
     private String name;
@@ -21,17 +21,17 @@ public class Role {
         super();
     }
 
-    public Role(UUID id, String role) {
-        this.id = id;
+    public Role(UUID role_id, String role) {
+        this.role_id = role_id;
         this.name = role;
     }
 
     public UUID getId() {
-        return id;
+        return role_id;
     }
 
-    public void setId(UUID id) {
-        this.id = id;
+    public void setId(UUID role_id) {
+        this.role_id = role_id;
     }
 
     public String getName() {
@@ -47,18 +47,18 @@ public class Role {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Role role1 = (Role) o;
-        return Objects.equals(id, role1.id) && Objects.equals(name, role1.name);
+        return Objects.equals(role_id, role1.role_id) && Objects.equals(name, role1.name);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, name);
+        return Objects.hash(role_id, name);
     }
 
     @Override
     public String toString() {
         return "Role{" +
-                "id='" + id + '\'' +
+                "role_id='" + role_id + '\'' +
                 ", role='" + name + '\'' +
                 '}';
     }
