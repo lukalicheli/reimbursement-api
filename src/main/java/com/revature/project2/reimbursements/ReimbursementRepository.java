edf106dc4cd5c36a9c28de96c59a7f9f06bfe16f
NMooney <nmooney96@hotmail.com>
2022-09-28 16:03:21 -0400

    Reimbursement Function Implementation
    Initial build with Repository, Controller, and updated Service classes for Reimbursements

---
+++ src/main/java/com/revature/project2/reimbursements/ReimbursementRepository.java
@@ -0,0 +1,10 @@
+package com.revature.project2.reimbursements;
+
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReimbursementRepository extends JpaRepository<Reimbursement, UUID> {
+    boolean existsByPending(UUID status_id);
+}
---
+++ src/main/java/com/revature/project2/reimbursements/ReimbursementsController.java
@@ -0,0 +1,39 @@
+package com.revature.project2.reimbursements;
+
+import static com.revature.project2.common.SecurityUtils.enforceAuthentication;
+import static com.revature.project2.common.SecurityUtils.enforcePermissions;
+import java.time.LocalDateTime;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/reimb")
+public class ReimbursementsController {
+    
+    private static Logger logger = LogManager.getLogger(ReimbursementsController.class);
+
+    private final ReimbursementService reimbService;
+    
+    @Autowired
+    public ReimbursementsController(ReimbursementService reimbService){
+        this.reimbService=reimbService;
+    }
+    
+    
+    @GetMapping(produces = "application/json")
+    public List<ReimbursementResponse> getAllUsers(HttpServletRequest req) {
+        logger.info("A GET request was received by /users at {}", LocalDateTime.now());
+        HttpSession userSession = req.getSession(false);
+        enforceAuthentication(userSession);
+        enforcePermissions(userSession, "admin");
+        return reimbService.getAllReimbs();
+    }
+}
