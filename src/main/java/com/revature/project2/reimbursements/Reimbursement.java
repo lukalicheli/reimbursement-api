5c0b744d2b50233def6c1707c131a192e649ac67
Chaz Corlew <chaz.corlew@gmail.com>
2022-09-28 11:30:12 -0700

    refactored reimbursement.java to implement spring

--- src/main/java/com/revature/project2/reimbursements/Reimbursement.java
+++ src/main/java/com/revature/project2/reimbursements/Reimbursement.java
@@ -1,21 +1,51 @@
 package com.revature.project2.reimbursements;
 
-public class Reimbursement {
-    private String reimbID;
-    private double amount;
-    private String timeSub;
-    private String timeResolved;
-    private String description;
-    private int authorID;
-    private int resolverID;
-    private String statusID;
-    private String typeID;
 
-    public String getReimbID() {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Table
+public class Reimbursement {
+    @Id
+    @Column(name = "reimb_id")
+    private UUID reimbID;
+
+    @Column(nullable = false)
+    private double amount;
+
+    @Column(nullable = false)
+    private String submitted;
+
+    @Column
+    private String resolved;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(name = "author_id", nullable = false)
+    private UUID authorID;
+
+    @Column(name = "resolver_id")
+    private UUID resolverID;
+
+    @Column(name = "status_id", nullable = false)
+    private UUID statusID;
+
+    @Column(name = "type_id", nullable = false)
+    private UUID typeID;
+
+
+
+    public UUID getReimbID() {
         return reimbID;
     }
 
-    public void setReimbID(String reimbID) {
+    public void setReimbID(UUID reimbID) {
         this.reimbID = reimbID;
     }
 
@@ -27,20 +57,20 @@ public class Reimbursement {
         this.amount = amount;
     }
 
-    public String getTimeSub() {
-        return timeSub;
+    public String getSubmitted() {
+        return submitted;
     }
 
-    public void setTimeSub(String timeSub) {
-        this.timeSub = timeSub;
+    public void setSubmitted(String submitted) {
+        this.submitted = submitted;
     }
 
-    public String getTimeResolved() {
-        return timeResolved;
+    public String getResolved() {
+        return resolved;
     }
 
-    public void setTimeResolved(String timeResolved) {
-        this.timeResolved = timeResolved;
+    public void setResolved(String resolved) {
+        this.resolved = resolved;
     }
 
     public String getDescription() {
@@ -51,35 +81,63 @@ public class Reimbursement {
         this.description = description;
     }
 
-    public int getAuthorID() {
+    public UUID getAuthorID() {
         return authorID;
     }
 
-    public void setAuthorID(int authorID) {
+    public void setAuthorID(UUID authorID) {
         this.authorID = authorID;
     }
 
-    public int getResolverID() {
+    public UUID getResolverID() {
         return resolverID;
     }
 
-    public void setResolverID(int resolverID) {
+    public void setResolverID(UUID resolverID) {
         this.resolverID = resolverID;
     }
 
-    public String getStatusID() {
+    public UUID getStatusID() {
         return statusID;
     }
 
-    public void setStatusID(String statusID) {
+    public void setStatusID(UUID statusID) {
         this.statusID = statusID;
     }
 
-    public String getTypeID() {
+    public UUID getTypeID() {
         return typeID;
     }
 
-    public void setTypeID(String typeID) {
+    public void setTypeID(UUID typeID) {
         this.typeID = typeID;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Reimbursement that = (Reimbursement) o;
+        return Double.compare(that.amount, amount) == 0 && reimbID.equals(that.reimbID) && submitted.equals(that.submitted) && resolved.equals(that.resolved) && description.equals(that.description) && authorID.equals(that.authorID) && resolverID.equals(that.resolverID) && statusID.equals(that.statusID) && typeID.equals(that.typeID);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(reimbID, amount, submitted, resolved, description, authorID, resolverID, statusID, typeID);
+    }
+
+    @Override
+    public String toString() {
+        return "Reimbursement{" +
+                "reimbID=" + reimbID +
+                ", amount=" + amount +
+                ", submitted='" + submitted + '\'' +
+                ", resolved='" + resolved + '\'' +
+                ", description='" + description + '\'' +
+                ", authorID=" + authorID +
+                ", resolverID=" + resolverID +
+                ", statusID=" + statusID +
+                ", typeID=" + typeID +
+                '}';
     }
 }
