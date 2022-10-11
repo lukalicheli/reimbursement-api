46b83aacb7af8c50121138bf66c5dc36f59d4d2a
NMooney <nmooney96@hotmail.com>
2022-10-11 15:38:33 -0400

    Search for all Pending Reimbursements
    Functionality achieved

--- src/main/java/com/revature/project2/reimbursements/ReimbursementRepository.java
+++ src/main/java/com/revature/project2/reimbursements/ReimbursementRepository.java
@@ -1,5 +1,6 @@
 package com.revature.project2.reimbursements;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,4 +11,5 @@ public interface ReimbursementRepository extends JpaRepository<Reimbursement, UU
 //    boolean existsByPending(UUID status_id);
 
     Optional<Reimbursement> findReimbursementByReimbID(UUID id);
+    List<Reimbursement> findAllReimbursementByStatusID(int statusIDPending);
 }
