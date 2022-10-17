40426c02ebf5dbb451b69191517163764133e441
NMooney <nmooney96@hotmail.com>
2022-10-17 17:34:03 -0400

    Update Project2.java
    Removed unused imports and console log from last functionality implementation

--- src/main/java/com/revature/project2/Project2.java
+++ src/main/java/com/revature/project2/Project2.java
@@ -3,29 +3,18 @@ package com.revature.project2;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.logging.log4j.core.appender.RollingFileAppender;
-import org.apache.tomcat.jni.File;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.logging.log4j2.Log4J2LoggingSystem;
 import org.springframework.context.annotation.Bean;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
-import java.util.Date;
-import java.util.logging.Logger;
 
 @SpringBootApplication // implies @Configuration @ComponentScan and @AutoConfiguration
 public class Project2 {
 
     public static void main(String[] args) {
         SpringApplication.run(Project2.class, args);
-        System.out.println(doHashing("passw0rd"));
 
     }
     public static String doHashing(String password) {
